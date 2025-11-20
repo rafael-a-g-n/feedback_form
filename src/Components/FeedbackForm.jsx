ea8ceae4af5ee4cd7073d41b4b65484bfacdd2ef
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-20 17:22:28 +0000

    Implemented controlled inputs

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -22,9 +22,26 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        <input type="text" name="name" placeholder="Your Name" />
-        <input type="email" name="email" placeholder="Your Email" />
-        <textarea name="feedback" placeholder="Your Feedback"></textarea>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <textarea
+          name="feedback"
+          placeholder="Your Feedback"
+          value={formData.feedback}
+          onChange={handleChange}
+        ></textarea>
         <button type="submit">Submit Feedback</button>
       </form>
     </>
