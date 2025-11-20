c8d199037edbde20f5e99205045610755255a99b
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-20 17:16:57 +0000

    built feedback form UI

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import './FeedbackForm.css'; // Import CSS for styling
+import React, { useState } from "react";
+import "./FeedbackForm.css"; // Import CSS for styling
 
 const FeedbackForm = () => {
-
   return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
+      <nav>Tell Us What You Think</nav>
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input type="text" name="name" placeholder="Your Name" />
+        <input type="email" name="email" placeholder="Your Email" />
+        <textarea name="feedback" placeholder="Your Feedback"></textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
