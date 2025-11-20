eaf0aab6f38bcfe30abc3d1fd70a3e7758dcd2c1
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-20 17:26:53 +0000

    added submit handler and confirmation

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -16,10 +16,31 @@ const FeedbackForm = () => {
     });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const confirmationMessage = `
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Feedback: ${formData.feedback}
+    `;
+    const isConfirmed = window.confirm(
+      `Please confirm your details:\n\n${confirmationMessage}`
+    );
+    if (isConfirmed) {
+      console.log("Submitting feedback:", formData);
+      setFormData({
+        name: "",
+        email: "",
+        feedback: "",
+      });
+      alert("Thank you for your valuable feedback!");
+    }
+  };
+
   return (
     <>
       <nav>Tell Us What You Think</nav>
-      <form className="feedback-form">
+      <form className="feedback-form" onSubmit={handleSubmit}>
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         <input
